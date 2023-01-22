64a91348b5252bc1b416324693ddd4a6de9d4d5d
Amir Hakim <littlefast2@gmail.com>
2023-01-22 12:49:10 +0800

    Lua API Reference

---
+++ autoexec.lua
@@ -0,0 +1,23 @@
+lookan.print("Hello World from Lookan Lua Library!")
+
+lookan.add_inputevent_handler(function(scancode, shift, ctrl, alt, is_pressed)
+	lookan.print(scancode)
+
+end)
+
+lookan.add_fileopened_handler(function(filepath)
+
+end)
+
+lookan.add_fileclosed_handler(function(filepath)
+
+end)
+
+lookan.add_filesaved_handler(function(filepath)
+
+end)
+
+lookan.add_textchanged_handler(function(filepath, text)
+	lookan.print(filepath)
+	lookan.print(text)
+end)
