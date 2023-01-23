364a016b052492c232c53077f24c66cc90f1af11
Amir Hakim <littlefast2@gmail.com>
2023-01-23 11:01:21 +0800

    Update Main.gd

--- Main.gd
+++ Main.gd
@@ -6,17 +6,24 @@ signal file_saved(filepath)
 
 signal text_changed(filepath, text)
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect()
+	connect("file_opened",$Lua,"queue_fileopened_event")
+	connect("file_closed",$Lua,"queue_fileclosed_event")
+	connect("file_saved",$Lua,"queue_filesaved_event")
+	connect("text_changed",$Lua,"queue_textchanged_event")
 
+	connect("file_opened", $Editor, "set_open_file")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	$Editor.connect("focus_exited", self, "_on_editor_losefocus")
+	$FileSystem.connect("focus_exited", self, "_on_filesys_losefocus")
+	$Editor.grab_focus()
+
+func _on_editor_losefocus():
+	$FileSystem.grab_focus()
+
+func _on_filesys_losefocus():
+	$Editor.grab_focus()
+
+func _on_editor_textchanged(filepath, text):
+	pass
